a2a3cbcb96be25554bf8cc4b6637bcdbb07f561d
wadiukarunathilaka <wadiukarunathilaka@gmail.com>
2023-06-14 20:50:11 +0530

    trigger deploy

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hi, How are you 
+          From GitHub
         </p>
         <a
           className="App-link"
